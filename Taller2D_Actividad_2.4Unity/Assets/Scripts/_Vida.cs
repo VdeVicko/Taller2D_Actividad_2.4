66cea787ae27c9dfd4fc4d7f17e28dc3eb708c9d
Hollow4U <sanchezvs2004@gmail.com>
2023-05-17 19:05:45 -0500

    DavidCardenas_ChipVida/CoinPuntaje

---
+++ Taller2D_Actividad_2.4Unity/Assets/Scripts/_Vida.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class _Vida : MonoBehaviour
+{
+    [Header("Time Variables")]
+    [SerializeField] private float timer;
+    [SerializeField] private float maxTimer;
+
+    [Header("Life Variables")]
+    [SerializeField] private int _lifeAdd;
+    [SerializeField] private int _Life;
+    [SerializeField] private int _maxLife;
+
+    void Update()
+    {
+        LifeReduction();
+        vida();
+    }
+    private void vida()
+    {
+        if (_Life >= _maxLife)
+        {
+            _Life = _maxLife;
+        }
+        if(_Life <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void LifeReduction()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= maxTimer)
+        {
+            _lifeAdd--;
+            timer = 0;
+        }
+        if (_lifeAdd <= 1)
+        {
+            _lifeAdd = 1;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        {
+            if (collision.gameObject.CompareTag("Chip"))
+            {
+                _Life += _lifeAdd;
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
